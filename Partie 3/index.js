8086d4794474d2b01181fe41a0a1a7b0b8b9180c
Joris Vidéo <jorisvideo@outlook.fr>
2017-01-29 10:52:25 +0100

    ajout d'index.js part 3

---
+++ Partie 3/index.js
@@ -0,0 +1,218 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const weather = require("weather-js");
+const token = "MjUwMDA1MDI3MTUyNzIzOTY5.CxOi3g.T12XrjS8QW6qjTG6qwkIODRUNhI";
+const Wiki = require("wikijs");
+const express = require("express");
+var app = express();
+var yt = require("./youtube_plugin");
+var youtube_plugin = new yt();
+var AuthDetails = require("./auth.json");
+var RedisSessions = require("redis-sessions");
+var rs = new RedisSessions();
+var Music = require("./Music.js");
+var functionHelper = require('./functionHelpers.js');
+var ffmpeg = require("ffmpeg");
+var search = require('youtube-search'),
+music = new Music();
+var prefix = ".";
+var moment = require("moment");
+var mention = "<@1930903359700619264>";
+const opts = {
+  maxResults: 3,
+  key: AuthDetails.youtube_api_key
+};
+client.on("ready", () => {
+var memberCount = client.users.size;
+var servercount = client.guilds.size;
+	var servers = client.guilds.array().map(g => g.name).join(',');
+	console.log("--------------------------------------");
+console.log('[!]Connexion en cours... \n[!]Veuillez Patienté! \n[!]Les évenement sont après ! :)  \n[!]Les préfix actuelle:  ' + prefix + "\n[!]Mentions = " + mention + "\n[!]Nombre de membres: " + memberCount + "\n[!]Nombre de serveurs: " + servercount);
+});
+var messages = [];
+client.on('message', message => {
+   music.setVoiceChannel(message.member.voiceChannel);
+    var array_msg = message.content.split(' ');
+            messages.push(message);
+            switch (array_msg[0]) {
+        case ("#play") :
+            console.log("Play");
+            message.delete(message.author);
+            if (!music.getVoiceChannel()) return message.reply("Veuillez vous connectez en vocal !");
+            if (music.getTab(0) == null) return message.reply('Aucune musique, merci d\' en ajouté.');
+            else music.voice();
+            break;
+        case ("#pause") :
+            console.log("Pause");
+            message.delete(message.author);
+            if (!music.getVoiceChannel()) return message.reply("Veuillez vous connectez en vocal !");
+            if (music.getTab(0) == null) return message.reply('Aucune musique, merci d\' en ajouté.');
+            music.pause();
+            break;
+        case ("#resume") :
+            console.log("Resume");
+            message.delete(message.author);
+            if (!music.getVoiceChannel()) return message.reply("Veuillez vous connectez en vocal !");
+            if (music.getTab(0) == null) return message.reply('Aucune musique, merci d\' en ajouté.');
+            music.resume();
+            break;
+        case ("#stop") :
+            console.log("Stop");
+            message.delete(message.author);
+            if (!music.getVoiceChannel()) return message.reply("Veuillez vous connectez en vocal !");
+            if (music.getTab(0) == null) return message.reply('Aucune musique, merci d\' en ajouté.');
+            else music.stop();
+            message.reply("La queue à était vidé !");
+            break;
+        case ("#add") :
+            console.log("Add");
+            message.delete(message.author);
+            var link = message.content.split(' ');
+            link.shift();
+            link = link.join(' ');
+            search(link, opts, function(err, results) {
+                if(err) return console.log(err);
+                for (var y = 0; results[y].kind == 'youtube#channel'; y++);
+                message.channel.sendMessage(results[y].link);
+                music.setTabEnd(results[y].link);
+            });
+            break;
+        case ("#link") :
+            console.log("Link");
+            message.delete(message.author);
+            var link = message.content.split(' ');
+            link.shift();
+            link = link.join(' ');
+            console.log(link);
+            music.setTabEnd(link);
+            break;
+        case ("#volume") :
+            console.log("Volume");
+            message.delete(message.author);
+            var link = message.content.split(' ');
+            link.shift();
+            link = link.join(' ');
+            music.volume(link/100);
+            message.reply("le volume et maintenant à :" + link);
+            break;
+        case ("#next") :
+            console.log("Next");
+            message.delete(message.author);
+            if (music.getI() < music.getLengthTab()) music.setI(this.i + 1);
+            if (music.getI() >= music.getLengthTab()) music.setI(0);
+            music.next();
+            break;
+    }	if (message.content === ("test")){
+	message.reply('test !');
+}else if (message.content.startsWith("!say")){
+    message.delete(message.author);
+    var say = message.content.substr(5);
+    message.reply(say);
+}
+
+    else if (message.content === ("!channel")){
+    const data = client.channels.get(message.channel.id);
+    moment.locale("fr");
+    var temps = moment(data.createdTimestamp).format("LLLL");
+    console.log(temps)
+    message.reply("\n" + "```javascript"+ "\n" + "Nom du channel: " + data.name + "\n" + "Type de channel: " + data.type + "\n" +
+    "Channel id: " + data.id + "\n" + "Topic: " + data.topic + "\n" + "Créer le: " + temps + "```" );
+    console.log("\n" + "**" + "Channel id: " + data.id + "**" );
+    console.log(data);
+    }
+ else if (message.content === ("bonjour")){
+	message.reply('bonjour à toi ');
+} else if(message.content.startsWith('!botname')){
+	client.user.setUsername(message.content.substr(9));
+} else if (message.content === "!stats") {
+    var memberCount = client.users.size;
+var servercount = client.guilds.size;
+	let m = " ";
+	m += 'je suis en compagnie de '+ memberCount +' membres';
+	m += 'je suis présent dans '+ servercount+' serveurs \n';
+	message.author.sendMessage(m).catch(console.log); 
+} 
+else if (message.content.startsWith("!méteo")){
+    var location = message.content.substr(6);
+    var unit = "C";
+    
+    try {
+        weather.find({search: location, degreeType: unit}, function(err, data) {
+            if(err) {
+                console.log(Date.now(), "DANGER", "Je ne peut pas trouvé d'information pour la méteo de " + location);
+                message.reply("\n" + "Je ne peut pas trouvé d'information pour la méteo de " + location);
+            } else {
+                data = data[0];
+               console.log("**" + data.location.name + " Maintenant : **\n" + data.current.temperature + "°" + unit + " " + data.current.skytext + ", ressentie " + data.current.feelslike + "°, " + data.current.winddisplay + " Vent\n\n**Prévisions pour demain :**\nHaut: " + data.forecast[1].high + "°, Bas: " + data.forecast[1].low + "° " + data.forecast[1].skytextday + " avec " + data.forecast[1].precip + "% de chance de precipitation.");
+               message.reply("\n" + "**" + data.location.name + " Maintenant : **\n" + data.current.temperature + "°" + unit + " " + data.current.skytext + ", ressentie " + data.current.feelslike + "°, " + data.current.winddisplay + " Vent\n\n**Prévisions pour demain :**\nHaut: " + data.forecast[1].high + "°, Bas: " + data.forecast[1].low + "° " + data.forecast[1].skytextday + " avec " + data.forecast[1].precip + "% de chance de precipitation.");
+            }
+        });
+    } catch(err) {
+        console.log(Date.now(), "ERREUR", "Weather.JS a rencontré une erreur");
+        message.reply("Idk pourquoi c'est cassé tbh :(");
+        }
+    }
+else if (message.content.startsWith("!wiki")){
+            if(!message.content.substr(5)) {
+                console.log(Date.now(), "DANGER", "Vous devez fournir un terme de recherche.");
+                message.reply("Vous devez fournir un terme de recherche.");
+                return;
+            }
+            var wiki = new Wiki.default();
+            wiki.search(message.content.substr(5)).then(function(data) {
+                if(data.results.length==0) {
+                    console.log(Date.now(), "DANGER","Wikipedia ne trouve pas ce que vous avez demandée : " + message.content.substr(5));
+                    message.reply("Je ne peut trouvé ce que vous voulez dans Wikipedia :(");
+                    return;
+                }
+                wiki.page(data.results[0]).then(function(page) {
+                    page.summary().then(function(summary) {
+                        if(summary.indexOf(" may refer to:") > -1 || summary.indexOf(" may stand for:") > -1) {
+                            var options = summary.split("\n").slice(1);
+                            var info = "Selectioné une options parmis celle-ci :";
+                            for(var i=0; i<options.length; i++) {
+                                info += "\n\t" + i + ") " + options[i];
+                            }
+                            message.reply(info);
+                            selectMenu(message.channel, message.author.id, function(i) {
+                                commands.wiki.process(Client, message, options[i].substring(0, options[i].indexOf(",")));
+                            }, options.length-1);
+                        } else {
+                            var sumText = summary.split("\n");
+                            var count = 0;
+                            var continuation = function() {
+                                var paragraph = sumText.shift();
+                                if(paragraph && count<3) {
+                                    count++;
+                                    message.reply(message.channel, paragraph, continuation);
+                                }
+                            };
+                            message.reply("**Trouvé " + page.raw.fullurl + "**", continuation);
+                        }
+                    });
+                });
+            }, function(err) {
+                console.log(Date.now(), "ERREUR","Impossible de se connecté a Wikipédia");
+                message.reply("Uhhh...Something went wrong :(");
+            });
+        
+} else if (message.content.startsWith('!youtube')){
+youtube_plugin.respond(message.content, message.channel , client);
+}
+});
+
+app.get('/', function (req, res) {
+    var obj = new Object();
+    obj.test = "Test moi";
+    obj.rep = "test réussi !";
+    var json = JSON.stringify(obj);
+    res.send(json);
+});
+
+app.get('/playlist', function (req, res) {
+    var json = JSON.stringify(music.tab);
+    res.send(json);
+});
+
+app.listen(AuthDetails.port);
+client.login(token)
